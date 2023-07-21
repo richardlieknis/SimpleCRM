7bb5db27f9a1f34b53ad2f4ed4558f3daa94b094
Richard Lieknis <richard.lieknis@web.de>
2023-07-21 22:43:14 +0200

    loading spinner added

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -8,17 +8,20 @@ import { AnyObject } from 'chart.js/types/basic';
 })
 export class NewsComponent implements OnInit {
   newsData: any = [];
+  isLoading: boolean = true;
 
   async ngOnInit(): Promise<void> {
     const apikey = 'c885cdb68f5d65089cb1ca9cd5c2a7ec';
     const category = 'technology';
     const url = 'https://gnews.io/api/v4/top-headlines?category=' + category + '&lang=en&country=us&max=10&apikey=' + apikey;
+    this.isLoading = true;
 
     await fetch(url)
       .then((response) => {
         return response.json();
       })
       .then((data) => {
+        this.isLoading = false;
         this.newsData = data.articles;
       });
   }
